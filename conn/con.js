0e98aaf507437343334fe2fafb4f8aab879dfc0e
nyambeni <53510131+nyambeni@users.noreply.github.com>
2020-01-29 11:56:15 +0200

    Update con.js

---
+++ conn/con.js
@@ -0,0 +1,31 @@
+const mysql = require('mysql');
+const express = require('express');
+
+
+const mysqlConn =mysql.createConnection({
+
+  host:'localhost',
+  user:'root',
+  password:'',
+  database:'icep2020'
+
+})
+
+
+mysqlConn.connect((err)  =>{
+
+if(!err)
+
+console.log('db connection succeed');
+
+
+else
+
+
+console.log('db connection failed');
+
+
+});
+
+
+module.exports =mysqlConn;
